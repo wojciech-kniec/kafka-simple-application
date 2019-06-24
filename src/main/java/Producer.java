609218f91cdfceca47e41b4a267fa3ad4d7d66ba
wojciech-kniec <kniecwojciech@gmail.com>
2019-06-24 23:37:00 +0200

    Producer Properties added

--- src/main/java/Producer.java
+++ src/main/java/Producer.java
@@ -1,10 +1,30 @@
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
 
 public class Producer {
     public static void main(String[] args) {
         final KafkaProducer<String, String> mProducer;
         final Logger logger = LoggerFactory.getLogger(Producer.class);
+
+    }
+
+    Producer(String bootstrapServer) {
+        Properties props = producerProps(bootstrapServer);
+        mProducer = new KafkaProducer<>(props);
+    }
+
+    private Properties producerProps(String bootstrapServer){
+        String serializer = StringSerializer.class.getName();
+        Properties props = new Properties();
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, serializer);
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, serializer);
+
+        return props;
     }
 }
