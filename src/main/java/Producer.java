f212578ed9c150572ae7ea996ed10b7115ca7199
wojciech-kniec <kniecwojciech@gmail.com>
2019-06-24 23:30:35 +0200

    Producer class added

--- src/main/java/Producer.java
+++ src/main/java/Producer.java
@@ -1,2 +1,10 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Producer {
+    public static void main(String[] args) {
+        final KafkaProducer<String, String> mProducer;
+        final Logger logger = LoggerFactory.getLogger(Producer.class);
+    }
 }
